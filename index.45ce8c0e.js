bc0692c05015dae5a453995a1984def860bde45f
Artem Shuhailo <artem0462@gmail.com>
2023-12-12 20:34:12 +0200

    make build

---
+++ index.45ce8c0e.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".gallery__list");e.addEventListener("click",function(t){t.preventDefault();var l=t.target.closest(".list-item__link"),n=document.getElementById("largeImg");l&&e.contains(l)&&(n.src=l.href)});//# sourceMappingURL=index.45ce8c0e.js.map
+
+//# sourceMappingURL=index.45ce8c0e.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const gallery = document.querySelector(".gallery__list");
+gallery.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const target = e.target.closest(".list-item__link");
+    const bigImage = document.getElementById("largeImg");
+    if (!target || !gallery.contains(target)) return;
+    bigImage.src = target.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
